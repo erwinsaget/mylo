56072c6285dd748f162ebe79f2152bff1f3ec3ac
Erwins <erwinssaget@gmail.com>
2019-04-12 19:03:19 -0400

    styling homepage

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import './Home.css';
 
 class Home extends Component {
   render() {
     return (
-      <p>Home</p>
+      <div className="home-page">
+        <div className="home-container">
+          <img className="logo" src="/mylo_logo.png" alt="Mylo Logo" />
+        </div>
+        <form className="add-todo">
+          <input className="home-input rounded-full" />
+          <button className="rounded-full home-button">Add Task</button>
+        </form>
+      </div>
     )
   }
 }
